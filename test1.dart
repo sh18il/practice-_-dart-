c954ba9a36e49dfe0e376167d55de87d407b7ed4
sh18il <sshibil379@gmail.com>
2024-01-15 09:16:54 -0800

    frist commmit

---
+++ test1.dart
@@ -0,0 +1,8 @@
+void main() {
+// Ask the user for a number. Depending on whether the number is even or odd, print   out an appropriate message to the user.(eg: user enter 4, the output “even“).
+
+  num a = 3;
+
+  String b = (a % 2 == 0) ? 'even' : 'odd';
+  print(b);
+}
