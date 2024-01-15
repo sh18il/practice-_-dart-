33fe2620735563340675a6ef720b532b0c80d817
sh18il <sshibil379@gmail.com>
2024-01-15 09:34:09 -0800

    update

--- test1.dart
+++ test1.dart
@@ -5,4 +5,19 @@ void main() {
 
   String b = (a % 2 == 0) ? 'even' : 'odd';
   print(b);
+
+  // Accept two inputs from the user and output its sum
+
+  num c = 2;
+  num sum = c + a;
+  print(sum);
+
+  // )Take a list, say for example this one:
+  List<int> d = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  for (var i = 0; i < d.length; i++) {
+    if (d[i] < 5) {
+      print(d[i]);
+    }
+  }
 }
